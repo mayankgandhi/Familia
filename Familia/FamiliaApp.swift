003b3f249e3d68078d8fdf130ef472f82c22930e
Mayank <mayankgandhi50@gmail.com>
2023-07-19 16:34:21 +0530

    Initial Commit

---
+++ Familia/FamiliaApp.swift
@@ -0,0 +1,20 @@
+//
+//  FamiliaApp.swift
+//  Familia
+//
+//  Created by Mayank Gandhi on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FamiliaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
