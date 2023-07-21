4de98d71e0d5fc4545f833f8fceca7f43129de6e
Mayank Gandhi <mayankgandhi50@gmail.com>
2023-07-22 00:56:24 +0530

    Added Amplify api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "familia": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
